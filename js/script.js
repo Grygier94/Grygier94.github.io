6340f2e29c41723c10683c00c078da58d83f56aa
Marcin Grygierczyk <kwaska1000@gmail.com>
2016-07-22 00:07:33 +0200

    Footer added.

--- js/script.js
+++ js/script.js
@@ -16,22 +16,19 @@ function initMap() {
     });
 }
 
-function detectBrowser() {
-    var useragent = navigator.userAgent;
-    var mapdiv = document.getElementById("map");
-
-    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
-        mapdiv.style.width = '100%';
-        mapdiv.style.height = '100%';
-    } else {
-        mapdiv.style.width = '600px';
-        mapdiv.style.height = '800px';
-    }
-
-    
-}
-
 $(document).ready(function () {
     initMap();
-    detectBrowser();
 });
+
+//function detectBrowser() {
+//    var useragent = navigator.userAgent;
+//    var mapdiv = document.getElementById("map");
+
+//    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
+//        mapdiv.style.width = '100%';
+//        mapdiv.style.height = '100%';
+//    } else {
+//        mapdiv.style.width = '600px';
+//        mapdiv.style.height = '800px';
+//    }
+//}
