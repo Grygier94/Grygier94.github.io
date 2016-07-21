0e2f92cc78b88d99cafd026db5aa22612a188b58
Marcin Grygierczyk <kwaska1000@gmail.com>
2016-07-21 22:03:59 +0200

    Testing api

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var map;
 function initMap() {
 
+  
+
     myLatLng = { lat: 50.085277, lng: 19.140868 };
 
     map = new google.maps.Map(document.getElementById('map'), {
@@ -15,4 +17,19 @@ function initMap() {
         map: map,
         title: 'Bierun, Skalna 7'
     });
+}
+
+function detectBrowser() {
+    var useragent = navigator.userAgent;
+    var mapdiv = document.getElementById("map");
+
+    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
+        mapdiv.style.width = '100%';
+        mapdiv.style.height = '100%';
+    } else {
+        mapdiv.style.width = '600px';
+        mapdiv.style.height = '800px';
+    }
+
+    
 }
