c76d082b7073007a86d2d4e87254becf7375c689
Marcin Grygierczyk <kwaska1000@gmail.com>
2016-07-21 15:24:32 +0200

    Contact map added.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-$(document).ready(function () {
+var map;
+function initMap() {
 
-    $('#toggle-nav').click(function () {
+    myLatLng = { lat: 50.085277, lng: 19.140868 };
 
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 10,
+        scrollwheel: false,
+        fullscreenControl: true
     });
 
-});
+    marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Bierun, Skalna 7'
+    });
+}
